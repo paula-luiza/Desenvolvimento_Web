b1a47b540bf051072b831384587b1e0c2efde4ad
paula-luiza <142267093+paula-luiza@users.noreply.github.com>
2024-05-06 11:21:04 -0300

    Add files via upload

---
+++ arrays-automatico/script.js
@@ -0,0 +1,43 @@
+console.log("Usando forEach", "-----------------------------------------------------------");
+
+dados.forEach(
+    (ob) => {
+        console.log(ob.nome)
+    }
+)
+
+console.log("Usando while", "-----------------------------------------------------------");
+
+let indice = 0;
+
+while (indice < dados.length){
+    console.log(`A posição de ${dados[indice].nome} é ${dados[indice].posicao}`);
+    indice++;
+}
+
+console.log("Usando for clássico", "-----------------------------------------------------------");
+
+for (let contador = 0; contador < dados.length; contador++){
+    console.log(`A altura de ${dados[contador].nome} é ${dados[contador].altura}`);
+}
+
+console.log("usando for...of", "-----------------------------------------------------------");
+
+for (let atleta of dados){
+    console.log(`A nome completo de ${atleta.nome} é ${atleta.nome_completo}`);
+}
+
+
+const atleta = dados[10];
+
+const conteudo = document.getElementById("conteúdo");
+
+conteudo.innerHTML = `
+    <div class='card'>
+        <img src='${atleta.imagem}' alt='Foto de ${atleta.nome}'>
+        <p>${atleta.posicao}</p>
+        <p class='nome'>${atleta.nome}</p>
+        <p class='descricao'>${atleta.descricao}</p>
+        <p class='nascimento'>${atleta.nascimento}</p>
+    </div>
+`;
